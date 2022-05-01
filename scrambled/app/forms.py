86d1b42ac4e18cd6e7199be66421b031ef3a1833
benvarian <88700858+benvarian@users.noreply.github.com>
2022-05-02 00:27:03 +0800

    game template added

--- scrambled/app/forms.py
+++ scrambled/app/forms.py
@@ -44,7 +44,7 @@ class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
 
 class PostForm(FlaskForm):
-    post = TextAreaField('Say something', validators=[
+    post = TextAreaField('Say something:', validators=[
         DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
 
